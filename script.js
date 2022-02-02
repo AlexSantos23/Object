6afd99cb2d9be99bb934eafd7dbc8c9cacb8e021
Alex <alexsantosdasilva1133@gmail.com>
2022-02-02 00:12:33 -0300

    Trabalho Object Alex

---
+++ script.js
@@ -0,0 +1,16 @@
+let dados= {
+    nome: "Alex",
+    idade: 21,
+    cabelo: "Preto"
+}
+
+dados.estiloMusical = "Rock"
+
+delete dados.cabelo
+
+console.log(dados)
+
+
+let cadastro = {nome:"Alex", idade:"21", telefone:"(00)000000000", amigos:["Liv", "Landi", "Gabriel", "Nathan"]}
+
+console.log(cadastro.amigos[0])
